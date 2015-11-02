b8786b09a7f1c03c0a3bab9fe02da53b98c12a85
Matt Seto <matthew.y.seto@gmail.com>
2015-11-02 10:33:06 -0800

    Major changes

--- TCSS372_A3.c
+++ TCSS372_A3.c
@@ -9,60 +9,39 @@
 */
 
 
+//*******NOTES*****
+/*
+	BR Flags are zeroed by the ALU each time it starts
+	
+	Flags are set appropriately on exit from the ALU
+	
+	
+	Overflow:
+	1 - If the signs of the two operands are DIFFERENT before the op, there is NEVER OVERFLOW
+	2 - If the signs are the same before the op, IF THE OUTPUT SIGN IS OPPOSITE, then there MUST HAVE BEEN OVERFLOW (therefore, set overflow)
+	 
+	Carry: 
+	adding 32 bits and 32 bits, you need to check for carry (overflow)
+	
+	Branching: 
+	When you are incrementing PC and offset etc, 
+		DO NOT USE THE ADD INSTRUCTION, because that will reset the branch condition flags
+		& we want to preserve those flags
+		
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-//#include <curses.h>
-
-//constants for the FSM
-#define FETCH 0
-#define DECODE 1
-#define EXECUTE 2
-
-//general exit Microstate
-#define EXIT INT_MAX //large number higher than all microstates
-
-
-//FETCH microstates
-#define FETCH1 0
-#define FETCH2 1
-#define FETCH3 2
-#define FETCH4 3
-
-//DECODE microstate
-#define DECODE1 0
-
-//EXECUTE microstates
-#define ADD 0
-#define NAND 1 
-#define ADDI 2
-#define LW 3
-#define SW 4
-#define BEQ 5
-#define JALR 6
-#define HALT 7
-
-//prototype for getting user to press a key to continue
-int pressAnyKey();
+#include "TCSS372_A3.h"
 
 
 int main (int argc, char* argv[]){
 
-	//int variables for registers
-	int PC = 0;
-	int IR = 0;
-	int MAR = 0;
-	int A = 0;
-	int B = 0;
-	int RX = 0;
-	int RY = 0;
-	int RZ = 0;
-	int immediate = 0;
-	int state = FETCH;
-	int microstate = FETCH1;
 	
-	int instruction = 0;
-	int opcode = 0;
+	//create the CPU
+	struct CPU* myCPU = create_CPU();
 
 	//while the state is not halt, continue looping through
 	 while(state != HALT){
@@ -72,12 +51,10 @@ int main (int argc, char* argv[]){
 				while(microstate < EXIT) {
 					switch(microstate) {
 						case FETCH1 :
-							pressAnyKey();
-							//printf("FETCH1\n");
 							//MAR <- PC
-							MAR = PC;
+							myCPU->MAR = myCPU->PC;
 							//A <- PC
-							A = PC;
+							myCPU->ALUA = myCPU->PC;
 							
 							//increment to the next microstate
 							microstate = FETCH2;
@@ -85,16 +62,13 @@ int main (int argc, char* argv[]){
 							break;
 						
 						case FETCH2 :
-							pressAnyKey();
-							//printf("FETCH2\n");
-							//load IR with instruction
+
 							//scan the hex value that defines instruction and specific variables
 							printf("Enter hex instruction: \n");
-							scanf("%x", &instruction);
-							//printf("%x\n", instruction);
+							scanf("%X", &instruction);
 							
 							//IR <- M[MAR] //input a number from 0-7 representing an instruction
-							IR = instruction;
+							myCPU->IR = instruction;
 							
 							//increment to next microstate
 							microstate = FETCH3;
@@ -102,10 +76,9 @@ int main (int argc, char* argv[]){
 							break;
 							
 						case FETCH3 :
-							pressAnyKey();
 							//printf("FETCH3\n");
 							//PC<- A +1 increment the PC
-							PC = A+1;
+							myCPU->PC = myCPU->ALUA+1;
 							
 							//increment the microstate
 							microstate = FETCH4;
@@ -113,7 +86,6 @@ int main (int argc, char* argv[]){
 							break;
 						
 						case FETCH4 :
-							pressAnyKey();
 							//printf("FETCH4\n");
 							//increment the state
 							state = DECODE;
@@ -127,146 +99,367 @@ int main (int argc, char* argv[]){
 			case DECODE :
 				//reset the microstate
 				microstate = DECODE1;
-				//while(microstate < EXIT) {
-					//printf("ENTERED DECODE\n");
 					
-					
-					//switch statement for j-type/i-type etc...instructions
-					//decode what type of instruction you are in
-					//mask the instruction hex (8 hex digits) with F000000 (7 hex digits)
-					//to get only the first digit
-					opcode = instruction & 0xF0000000;
-					//shift to the least significant space
-					opcode = opcode >> 28;
-					printf("Opcode: %x\n", opcode);
+					//shift the instruction to read only the first 5 digits
+					opcode = myCPU->IR >> 27;
 					
 					//switch through the appropriate microstate
 					switch(opcode) {
-						case 0x0 :
+						case 0x01 :
+							printf("LDI\n");
+							microstate = LDI;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x02 :
+							printf("LD\n");
+							microstate = LD;
+							DecodeFormat2(myCPU->IR);
+							break; 
+						//case 0x03 :
+						//	microstate = ST;
+						//	break;
+						case 0x04 :
+							printf("ST\n");
+							microstate = ST;
+							DecodeFormat2(myCPU->IR);
+							break;
+						case 0x05 :
+							microstate = MOV;
+							DecodeFormat3(myCPU->IR);
+							break;
+						case 0x06 :
+							microstate = PUSH;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x07 :
+							microstate = POP;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x08 :
+							printf("ADD\n");
 							microstate = ADD;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							RZ = instruction & 0x0000000F;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x1 :
-							microstate = NAND;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							RZ = instruction & 0x0000000F;
+						case 0x09 :
+							microstate = SUB;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x2 :
-							microstate = ADDI;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							immediate = 0x000FFFFF;
+						case 0x0A :
+							microstate = AND;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x3 :
-							microstate = LW;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							immediate = 0x000FFFFF;
+						case 0x0B :
+							microstate = OR;
+							DecodeFormat3(myCPU->IR);
+							break;		
+						case 0x0C :
+							microstate = NOT;
+							DecodeFormat2(myCPU->IR);
 							break;
-						case 0x4 :
-							microstate = SW;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							immediate = 0x000FFFFF;
+						case 0x0D :
+							microstate = XOR;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x5 :
-							microstate = BEQ;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
-							immediate = 0x000FFFFF;
+						case 0x0E :
+							microstate = SHL;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x6 :
-							microstate = JALR;
-							RX = ((instruction & 0x0F000000) >> 24);
-							RY = ((instruction & 0x00F00000) >> 20);
+						case 0x0F :
+							microstate = SHR;
+							DecodeFormat3(myCPU->IR);
 							break;
-						case 0x7 :
+						case 0x10 :
+							microstate = BR;
+							DecodeFormat4(myCPU->IR);
+							break;
+						case 0x11 :
+							printf("BRZ\n");
+							microstate = BRZ;
+							DecodeFormat4(myCPU->IR);
+							break;
+						case 0x12 :
+							microstate = BRN;
+							DecodeFormat4(myCPU->IR);
+							break;
+						case 0x13 :
+							microstate = BRC;
+							DecodeFormat4(myCPU->IR);
+							break;			
+						case 0x14 :
+							microstate = BRO;
+							DecodeFormat4(myCPU->IR);
+							break;
+						case 0x15 :
+							microstate = JSR;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x16 :
+							microstate = JSRZ;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x17 :
+							microstate = JSRN;
+							DecodeFormat1(myCPU->IR);
+							break;
+						case 0x18 :
+							microstate = DI;
+							break;
+						case 0x19 :
+							microstate = EI;
+							break;
+						case 0x1A :
+							microstate = RET;
+							break;
+						case 0x1B :
+							microstate = RETI;
+							break;			
+						case 0x1C :
+							microstate = TRAP;
+							break;
+						case 0x1D :
 							microstate = HALT;
-							break;							
+							break;
+						case 0x00 :
+							microstate = NOP;
+							break;	
 					}
-					//set the appropriate register values (taken from the input)
 					
-					//increment the state and microstate
 					state = EXECUTE;
-					//microstate = EXIT;
-				//}
-				//printf("EXITED DECODE\n");		
+							
 				
 			case EXECUTE :
 				while(microstate < EXIT){
-					//printf("ENTERED EXECUTE\n");
 					
 					//switch statement to handle the appropriate microstate
-					switch(microstate) {
+					 switch(microstate) {
+						case LDI :
+							printf("In the LDI microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;	
+						case LD :
+							printf("In the LD microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case ST :
+							printf("In the ST microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case MOV :
+							printf("In the MOV microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case PUSH :
+							printf("In the PUSH microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case POP :
+							printf("In the POP microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
 						case ADD :
 							printf("In the ADD microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case NAND :
-							printf("In the NAND microstate\n");
+						case SUB :
+							printf("In the SUB microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case ADDI :
-							printf("In the ADDI microstate\n");
+						case AND :
+							printf("In the AND microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case LW :
-							printf("In the LW microstate\n");
+						case OR :
+							printf("In the OR microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case SW :
-							printf("In the SW microstate\n");
+						case XOR :
+							printf("In the XOR microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case BEQ :
-							printf("In the BEQ microstate\n");
+						case SHL :
+							printf("In the SHL microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
-						case JALR :
-							printf("In the JALR microstate\n");
+						case SHR :
+							printf("In the SHR microstate\n");
 							state = FETCH;	
 							instruction = 0;
 							microstate = EXIT;
 							break;
+						case BR :
+							printf("In the BR microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case BRZ :
+							printf("In the BRZ microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case BRN :
+							printf("In the BRN microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case BRC :
+							printf("In the BRC microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case BRO :
+							printf("In the BRO microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case JSR :
+							printf("In the JSR microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case JSRZ :
+							printf("In the JSRZ microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						case JSRN :
+							printf("In the JSRN microstate\n");
+							state = FETCH;	
+							instruction = 0;
+							microstate = EXIT;
+							break;
+						
 						case HALT :
 							printf("In the HALT microstate\n");
-							state = HALT;
+							state = HALT;	
+							instruction = 0;
 							microstate = EXIT;
 							break;
 					}
 
-					printf("PC: %d,\t IR: %x,\t MAR: %d,\t A: %d,\t B: %d,\t RX: %d,\t RY: %d,\t RZ: %d,\n", PC, IR, MAR, A, B, RX, RY, RZ);	
+					printDebugMonitor(myCPU);
+					state = FETCH;
+					microstate = EXIT;
+					
 				}		
 		}		
 	}
 	printf("\n");
+	CPU_Destroy(myCPU);
 	return (0);
 	}
 	
 	
-	//function to get user to press a key before continuing
-	int pressAnyKey() {
+	//*** FUNCTIONS
+	
+	//function to create a CPU struct
+	struct CPU *create_CPU(){
+
+	struct CPU *theCPU = malloc(sizeof(struct CPU));
+	theCPU->PC = 0;
+	theCPU->MAR = 0;
+	theCPU->MDR = 0;
+	theCPU->IR = 0;
+	theCPU->ALUA = 0;
+	theCPU->ALUB = 0;
+	theCPU->ALUResult = 0;
+	
+	return theCPU;
+	}
+	
+	//function to destroy a CPU struct
+	void CPU_Destroy(struct CPU *theCPU){
+		free(theCPU);
+	}	
+	
+	//function to decode format 1 instructions
+	void DecodeFormat1(unsigned int instr){
+		//*** FORMAT 1 ***
+		//to get RD, mask with 0x07800000
+		RD = ((instr & 0x07800000) >> 23);
+		//mask the opcode and register, leave only the immediate
+		immediate = (instr & 0x007FFFFF);
+	}
+	
+	//function to decode format 2 instructions
+	void DecodeFormat2(unsigned int instr){
+		//*** FORMAT 2 ***
+		//to get RD, mask with 0x07800000
+		RD = ((instr & 0x07800000) >> 23);
+		//to get RS1, mask with 0x00780000
+		RS1 = ((instr & 0x00780000) >> 19);
+		//mask the opcode, register and base register, leave the imm.
+		immediate = (instruction & 0x0007FFFF);
+	}
+	
+	//function to decode format 3 instructions
+	void DecodeFormat3(unsigned int instr){
+		//****FORMAT 3
+		//to get RD, mask with 0x07800000
+		RD = ((instr & 0x07800000) >> 23);
+		//to get RS1, mask with 0x00780000
+		RS1 = ((instr & 0x00780000) >> 19);
+		//to get RS2, mask with 0x00078000
+		RS2 = ((instr & 0x00078000) >> 15);
+	}
+	
+	//function to decode format 4 instructions
+	void DecodeFormat4(unsigned int instr){
+		// *** FORMAT 4 ***
+		//mask to preserve bits 27-0
+		immediate = (instruction & 0x07FFFFFFF);
+	}
+
+	
+	//function to display the debug monitor
+	void printDebugMonitor(struct CPU *theCPU){
+		int j;
+		printf("\tSC-4 Debug Monitor\n\n");
+		printf("Instruction registers: RD: %d, RS1: %d, RS2: %d\n\n", RD, RS1, RS2);
+		printf("Register File:\t\t\t\t\t Memory Dump:\n");
+		for(j=0; j<17; j++){
+			printf("%d:\t%08X\t\t\t\t 00000000:\t%08X\n", j, theCPU->registers[j], systemMemory[j]);
+		}
+		printf("\n");
+		printf("PC: %08X\t IR: %08X\t SW: %08X\t\n", theCPU->PC, theCPU->IR, 0);
+		printf("MAR: %08X\t MDR: %08X\t ALU.A: %08X\t ALU.B: %08X\t ALU.R: %08X", theCPU->MAR, theCPU->MDR, theCPU->ALUA, theCPU->ALUB, theCPU->ALUResult);
+		
+		printf("\n\n");
 		printf("Press Any Key to Continue: \n");
 		
 		//getch();
 		//getch() is not available on OSX, this workaround was found on stack overflow
 		// http://stackoverflow.com/questions/267250/equivalent-to-windows-getch-for-mac-linux-crashes
 		system("read -n1 -p ' ' key"); 
-		
-		return 0;
+	
 	}
---
+++ TCSS372_A3.h
@@ -0,0 +1,102 @@
+//constants for the FSM
+#define FETCH 0
+#define DECODE 1
+#define EXECUTE 2
+
+//general exit Microstate
+#define EXIT INT_MAX //large number higher than all microstates
+
+
+//FETCH microstates
+#define FETCH1 0
+#define FETCH2 1
+#define FETCH3 2
+#define FETCH4 3
+
+//DECODE microstate
+#define DECODE1 0
+
+//EXECUTE microstates
+#define LDI 0x1
+#define LD 0x2 
+//#define LD-2 3
+#define ST 0x04
+#define MOV 0x05
+#define PUSH 0x06
+#define POP 0x07
+#define ADD 0x08
+#define SUB 0x09
+#define AND 0x0A
+#define OR 0x0B
+#define NOT 0x0C
+#define XOR 0x0D
+#define SHL 0x0E
+#define SHR 0x0F
+#define BR 0x10
+#define BRZ 0x11
+#define BRN 0x12
+#define BRC 0x13
+#define BRO 0x14
+#define JSR 0x15
+#define JSRZ 0x16
+#define JSRN 0x17
+#define DI 0x18
+#define EI 0x19
+#define RET 0x1A
+#define RETI 0x1B
+#define TRAP 0x1C
+#define HALT 0x1D
+#define NOP 0x00
+
+//struct for a CPU
+typedef struct CPU{
+	//PC
+	unsigned int PC;
+	//MAR
+	unsigned int MAR;
+	//MDR 
+	unsigned int MDR;
+	//IR
+	unsigned int IR;
+	//ALU Registers
+	unsigned int ALUA;
+	unsigned int ALUB;
+	unsigned int ALUResult;
+	//array of registers 0-15
+	unsigned int registers[16];
+} CPU;
+
+//int variables for registers
+unsigned int RD = 0; 	//destination register
+unsigned int RS1 = 0; 	//source register 1
+unsigned int RS2 = 0; 	//source register 2
+unsigned int RB = 0; 	//base register
+unsigned int immediate = 0;
+	
+//initialize state
+int state = FETCH;
+int microstate = FETCH1;
+	
+unsigned int instruction = 0;
+unsigned int opcode = 0;
+	
+//array of system memory
+unsigned int systemMemory[100];
+
+
+//***Function prototypes
+
+//prototype to create a CPU
+struct CPU *create_CPU();
+
+//prototype to destroy the CPU
+void CPU_Destroy(struct CPU *theCPU);
+
+//prototype methods for decoding instructions format 1-4
+void DecodeFormat1(unsigned int instr);
+void DecodeFormat2(unsigned int instr);
+void DecodeFormat3(unsigned int instr);
+void DecodeFormat4(unsigned int instr);
+
+//prototype for printing the debug monitor
+void printDebugMonitor(struct CPU *theCPU);
